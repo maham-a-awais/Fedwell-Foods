abe947298b530f30f5bf6ad1726e0d65bf04e7cd
maham-a-awais <mahamawais888@outlook.com>
2025-03-21 21:11:20 +0500

    removed comment

--- fedwell-foods/src/components/custom/NavBar.tsx
+++ fedwell-foods/src/components/custom/NavBar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "./NavLink";
 
 export function NavBar({
@@ -10,6 +10,11 @@ export function NavBar({
   overviewRef: React.RefObject<HTMLDivElement | null>;
   contactRef: React.RefObject<HTMLDivElement | null>;
 }) {
+  const [homeRef, setHomeRef] = useState<React.RefObject<HTMLDivElement | null> | null>(null);
+
+  useEffect(() => {
+    setHomeRef({ current: document.body as HTMLDivElement });
+  }, []);
   return (
     <nav className="w-full bg-white py-4 shadow-md sticky top-0 z-50 border-none">
       <div className="container mx-auto flex  items-center px-4">
@@ -26,7 +31,7 @@ export function NavBar({
 
         {/* NAV LINKS */}
         <div className="flex space-x-4">
-          <NavLink text="Home" targetRef={{ current: document.body }} />
+          {homeRef && <NavLink text="Home" targetRef={homeRef} />}
           <NavLink text="Overview" targetRef={overviewRef} />
           <NavLink text="Contact" targetRef={contactRef} />
         </div>
